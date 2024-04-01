5ca022177686300fcf4c6e8fe124f40a7fa0eb1f
mtarun02 <tmattikallI@gmail.com>
2024-04-01 00:48:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ from typing import Union
 app = FastAPI()
 print("hellohellohellohello")
 # CORS configuration
-#app.add_middleware(
-#    CORSMiddleware,
-#    allow_origins=["*"],  # Allow requests from all origins
-#    allow_credentials=True,
-#    allow_methods=["GET", "POST"],
-#    allow_headers=["*"],
-#)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow requests from all origins
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
 
 class FormData(BaseModel):
     fieldRace: str
