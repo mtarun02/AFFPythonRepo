c0fe8eec59bcff336c47635fca527bddda26fac5
mtarun02 <tmattikallI@gmail.com>
2024-04-21 14:56:27 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-
-
-
 from fastapi import FastAPI, Form, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
@@ -13,7 +10,7 @@ import pandas as pd
 from typing import Union
 
 app = FastAPI()
-print("hellohellohellohello")
+
 # CORS configuration
 app.add_middleware(
     CORSMiddleware,
@@ -25,13 +22,13 @@ app.add_middleware(
 
 class FormData(BaseModel):
     fieldRace: str
-    fieldAgeAtFirst: int
+    fieldAgeAtFirst: str
     fieldBisphosphonates: str
-    fieldDuration: int
+    fieldDuration: str
     fieldOtherMedication: str
-    fieldWeight: float
-    fieldHeight: float
-    fieldBMI: float
+    fieldWeight: str
+    fieldHeight: str
+    fieldBMI: str
     fieldPreviousNTF: str
     fieldParent: str
     fieldSmoker: str
@@ -39,37 +36,74 @@ class FormData(BaseModel):
     fieldRA: str
     fieldSO: str
     fieldAlcoholism: str
-    fieldMCT: float
-    fieldLCT: float
-    fieldOffset: float
-    fieldFMA: float
-    fieldFMD: float
-    fieldVO: float
-    fieldFMT: float
-    fieldDXABMD: float
-    fieldDXAlumbar: float
-    fieldFraxMajor: float
-    fieldFraxHip: float
+    fieldMCT: str
+    fieldLCT: str
+    fieldOffset: str
+    fieldFMA: str
+    fieldFMD: str
+    fieldVO: str
+    fieldFMT: str
+    fieldDXABMD: str
+    fieldDXAlumbar: str
+    fieldFraxMajor: str
+    fieldFraxHip: str
+
+  
 
 
-
-model = load_model("AFF_Prediction")
+model = load_model("AFF_Prediction2")
 input_model = create_model
 def predict (race,age,bisphosphonate,duration,other,weight,height,bmi,ntf,parent,smoke,steroid,ra,so,alcohol,mct,lct,offset,fma,fmd,vo,fmt,dxa1,dxa2,frax1,frax2):
+    
+    #fields are inputted as strings. If they are empty strings, set them as NaN so that they can be passed as empty floats and handled by model. 
+    if height == "":
+        height = "nan"
+    if weight == "":
+        weight = "nan"
+    if bmi == "":
+        bmi = "nan"
+    if age == "":
+        age = "nan"
+    if duration == "":
+        duration = "nan"
+    if mct == "":
+        mct = "nan"
+    if lct == "":
+        lct = "nan"
+    if offset == "":
+        offset = "nan"
+    if fma == "":
+        fma = "nan"
+    if fmd == "":
+        fmd = "nan"
+    if vo == "":
+        vo = "nan"
+    if fmt == "":
+        fmt = "nan"
+    if dxa1 == "":
+        dxa1 = "nan"
+    if dxa2 == "":
+        dxa2 = "nan"
+    if frax1 == "":
+        frax1 = "nan"
+    if frax2 == "":
+        frax2 = "nan"
+
     data = pd.DataFrame([[race,age,bisphosphonate,duration,other,weight,height,bmi,ntf,parent,smoke,steroid,ra,so,alcohol,mct,lct,offset,fma,fmd,vo,fmt,dxa1,dxa2,frax1,frax2]])
     data.columns = ['Race','Age at fracture (first hip)','Bisphosphonates 1','Duration (yrs)','Other medications 1','Weight (kg)','Height (cm)','BMI (Kg/m^2)','Previous non traumatic fracture','Parent fractured hip (mother or father)','Current smoker','Steroid Use (> 3 months)','Rheumatoid arthritis','History of secondary osteoporosis','Alcoholism (> 3 drinks per day)','Medial cortical thickness','Lateral cortical thickness','Offset','Femoral neck angle','Femural Head Diameter','Vertical Offset','Femoral neck thickness','DXA Femoral Neck #1 BMD (g/cm^2)','DXA L1-L4 BMD (g/cm^2)','FRAX Score - online calculator major fracture (%)','FRAX Score - online calculator hip fracture']
     predictions = predict_model(model,data=data)
-    return{'Prediction:': predictions["prediction_label"].iloc[0]}
+    return{'Prediction:': predictions["prediction_label"].iloc[0],"Probability of Prediction":predictions["prediction_score"].iloc[0]}
 
 
 
+#@app.post("/submit/")
 @app.post("/")
+
 async def submit_form(data: FormData):
-    print("SHIVAM AMIN2")
     try:
         form_data = data.dict()
 
-        
+        print(form_data.get('fieldRace'))
         print(predict(form_data.get('fieldRace'), form_data.get('fieldAgeAtFirst'), form_data.get('fieldBisphosphonates'), form_data.get('fieldDuration'), form_data.get('fieldOtherMedication'), form_data.get('fieldWeight'), form_data.get('fieldHeight'), form_data.get('fieldBMI'), form_data.get('fieldPreviousNTF'), form_data.get('fieldParent'), form_data.get('fieldSmoker'), form_data.get('fieldSteroid'), form_data.get('fieldRA'), form_data.get('fieldSO'), form_data.get('fieldAlcoholism'), form_data.get('fieldMCT'), form_data.get('fieldLCT'), form_data.get('fieldOffset'), form_data.get('fieldFMA'), form_data.get('fieldFMD'), form_data.get('fieldVO'), form_data.get('fieldFMT'), form_data.get('fieldDXABMD'), form_data.get('fieldDXAlumbar'), form_data.get('fieldFraxMajor'), form_data.get('fieldFraxHip')))
         AFF_prediction = predict(form_data.get('fieldRace'), form_data.get('fieldAgeAtFirst'), form_data.get('fieldBisphosphonates'), form_data.get('fieldDuration'), form_data.get('fieldOtherMedication'), form_data.get('fieldWeight'), form_data.get('fieldHeight'), form_data.get('fieldBMI'), form_data.get('fieldPreviousNTF'), form_data.get('fieldParent'), form_data.get('fieldSmoker'), form_data.get('fieldSteroid'), form_data.get('fieldRA'), form_data.get('fieldSO'), form_data.get('fieldAlcoholism'), form_data.get('fieldMCT'), form_data.get('fieldLCT'), form_data.get('fieldOffset'), form_data.get('fieldFMA'), form_data.get('fieldFMD'), form_data.get('fieldVO'), form_data.get('fieldFMT'), form_data.get('fieldDXABMD'), form_data.get('fieldDXAlumbar'), form_data.get('fieldFraxMajor'), form_data.get('fieldFraxHip'))
         #return JSONResponse(content={"Prediction":AFF_prediction})
